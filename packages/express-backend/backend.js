4e9f250816272fee7034567474d395be473a583a
Paul Motter <pmotter@calpoly.edu>
2025-01-16 10:19:54 -0800

    v0.5 Added DELETE and improved GET

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,6 +6,7 @@ const port = 8000;
 
 app.use(express.json());
 
+//GET / root endpoint
 app.get("/", (req, res) => {
     res.send("Hello World!");    
 });
@@ -14,57 +15,55 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
+//Data for /users endpoints.
 const users = {
     users_list: [
-      {
-        id: "xyz789",
-        name: "Charlie",
-        job: "Janitor"
-      },
-      {
-        id: "abc123",
-        name: "Mac",
-        job: "Bouncer"
-      },
-      {
-        id: "ppp222",
-        name: "Mac",
-        job: "Professor"
-      },
-      {
-        id: "yat999",
-        name: "Dee",
-        job: "Aspring actress"
-      },
-      {
-        id: "zap555",
-        name: "Dennis",
-        job: "Bartender"
-      }
+        {
+            id: "xyz789",
+            name: "Charlie",
+            job: "Janitor"
+          },
+          {
+            id: "abc123",
+            name: "Mac",
+            job: "Bouncer"
+          },
+          {
+            id: "ppp222",
+            name: "Mac",
+            job: "Professor"
+          },
+          {
+            id: "yat999",
+            name: "Dee",
+            job: "Aspring actress"
+          },
+          {
+            id: "zap555",
+            name: "Dennis",
+            job: "Bartender"
+          }
     ]
   };
 
-const findUserByName = (name) => {
-    return users.users_list.filter(
-        (user) => user.name === name
-    );
-};
+//GET /users
+app.get("/users", (req, res) => {
+    let result = users.users_list;
+    if (req.query.job != undefined){
+        result = result.filter((user) => user.job === req.query.job)
+    }
+    if (req.query.name != undefined){
+        result = result.filter((user) => user.name === req.query.name)
+    }
+    res.send(result);
+});
 
+//Function for GET /users/:id endpoint.
 const findUserById = (id) => {
     return users.users_list.find((user) => user.id === id);
 };
 
-app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined){
-        let result = findUserByName(name);
-        result = {users_list:result};
-        res.send(result);
-    } else {
-        res.send(users);
-    }
-});
-
+//GET /users/:id
 app.get("/users/:id", (req, res) => {
     const id = req.params.id;
     let result = findUserById(id);
@@ -76,3 +75,31 @@ app.get("/users/:id", (req, res) => {
     }
 });
 
+//Function for POST /users
+const addUser = (user) => {
+    users.users_list.push(user);
+    return user;
+};
+
+//POST /users
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
+
+//Function for DELETE /users/:id
+const deleteUser = (id) => {
+    return users.users_list = users.users_list.filter((user) => user.id !== id);
+};
+
+//DELETE /users/:id
+app.delete("/users/:id", (req, res) => {
+    const length = users.users_list.length;
+    deleteUser(req.params.id);
+    if (length === users.users_list.length){
+        res.status(404).send("Resource not found.")
+    } else {
+        res.send();
+    }
+});
