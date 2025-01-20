0391ad15058f7b7ebdbef5881f3aac86be932016
Paul Motter <pmotter@calpoly.edu>
2025-01-20 12:51:03 -0800

    3. Linking Frontend With Backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -45,11 +45,22 @@ function MyApp(){
     }
     //upadtes the list with another character/person.
     function updateList(person){
-        setCharacters([...characters, person]);
+        postUser(person)
+            .then(() => setCharacters([...characters, person]))
+            .catch((error) => console.log(error));
     }
 
     function fetchUsers() {
         const promise = fetch("http://localhost:8000/users");
+        return promise;
+    }
+
+    function postUser(person) {
+        const promise = fetch("Http://localhost:8000/users", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(person),
+        });
         return promise;
     }
 }
